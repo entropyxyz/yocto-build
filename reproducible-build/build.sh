545cc04b7115c468a102f76fbef48baa684ee22b
peg <ameba23@systemli.org>
2025-03-05 12:11:23 +0100

    Copy in binary in shell script

--- reproducible-build/build.sh
+++ reproducible-build/build.sh
@@ -10,8 +10,25 @@ git config --global color.ui true
 
 cd /build
 
+# Clone git repos
 repo init -u https://github.com/entropyxyz/yocto-build.git -b ${REVISION} -m tdx-base.xml
 repo sync
+
+# Download entropy-tss binary
+# TODO --header='Authorization: token $GITHUB_TOKEN'
+wget -O entropy-tss $ENTROPY_TSS_BINARY_URI
+
+# TODO unzip and get amd64 version
+
+# TODO sha256sum entropy-tss
+# then append `SRC_URI[sha256sum] = "$SHA256SUM"` to srcs/poky/meta-entropy-tss/recipes-core/entropy-tss/entropy-tss.bb
+#
+# Set executable permissions
+chmod a+x entropy-tss
+
+# Move file to binary location directory
+mv entropy-tss srcs/poky/meta-entropy-tss/recipes-core/entropy-tss/.
+
 
 source setup || true
 
